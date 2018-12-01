f5ec2e5443b846209647010c697360341f50d1c1
xiaobaoxixi <liyan.xiaobaoxixi@gmail.com>
2018-12-01 12:30:02 +0100

    changed: on public index page, link to css instead of scss; on React index, import scss instead of css. Added: basic wireframe

--- public/main.js
+++ public/main.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
   const otherContent = document.querySelector("#otherContent");
   console.log(otherContent);
+  const callToActionBtnS = document.querySelectorAll(".callToAction");
+  const userForms = document.querySelector("#userForms");
 }
 
-let checkStatus = setInterval(checkLocalStorage, 1000);
-function checkLocalStorage() {
+let checkStatus = setInterval(checkSessionStorage, 1000);
+function checkSessionStorage() {
   const user = window.sessionStorage.getItem("user");
   if (user !== null) {
     clearInterval(checkStatus);
